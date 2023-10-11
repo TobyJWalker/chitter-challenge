5fe510c6c3fc2498e20a12f9a57520a88fa22df3
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-11 16:42:50 +0100

    Tested integration of validator with database

--- lib/validator.py
+++ lib/validator.py
@@ -1,8 +1,7 @@
 import re
+from lib.model_definition import User, Peep
 
 class Validator:
-    def __init__(self):
-        from lib.model_definition import User, Peep
 
     def validate_email(self, email):
         if re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
@@ -33,3 +32,42 @@ class Validator:
             return False
         
         return True
+    
+    def validate_signup(self, email, username, password, password_confirmation):
+        errors = {'email': [], 'username': [], 'password': [], 'password_confirmation': []}
+        valid = True
+
+        if not self.validate_email(email):
+            errors['email'].append('Invalid email format')
+            valid = False
+        
+        if not self.validate_username(username):
+            errors['username'].append('Username must be at least 3 characters')
+            valid = False
+        
+        if not self.validate_password(password):
+            errors['password'].append('Password must be at least 8 characters')
+            errors['password'].append('Password must contain at least one number')
+            errors['password'].append('Password must contain at least one uppercase letter')
+            errors['password'].append('Password must contain at least one special character')
+            valid = False
+        
+        if password != password_confirmation:
+            errors['password_confirmation'].append('Passwords do not match')
+            valid = False
+        
+        try:
+            User.select().where(User.username == username).get()
+            errors['username'].append('Username already taken')
+            valid = False
+        except:
+            pass
+        
+        try:
+            User.select().where(User.email == email).get()
+            errors['email'].append('Email already taken')
+            valid = False
+        except:
+            pass
+        
+        return valid, errors
---
+++ tests/test_validator_integration.py
@@ -0,0 +1,25 @@
+from lib.validator import Validator
+from lib.model_definition import User, Peep
+from seeds.seed_db import seed_database
+
+def test_validate_signup():
+    seed_database()
+    vd = Validator()
+
+    attempt_1, _ = vd.validate_signup('twalker@outlook.com', 'twalker120', 'Password@1', 'Password@1')
+    assert attempt_1 == True
+
+    attempt_2, errors = vd.validate_signup('twalkergmail.com', 'hi', 'pass', 'password')
+    assert attempt_2 == False
+    assert 'Invalid email format' in errors['email']
+    assert 'Username must be at least 3 characters' in errors['username']
+    assert 'Password must be at least 8 characters' in errors['password']
+    assert 'Password must contain at least one number' in errors['password']
+    assert 'Password must contain at least one uppercase letter' in errors['password']
+    assert 'Password must contain at least one special character' in errors['password']
+    assert 'Passwords do not match' in errors['password_confirmation']
+
+    attempt_3, errors = vd.validate_signup('twalker@gmail.com', 'twalker', 'Password@1', 'Password@1')
+    assert attempt_3 == False
+    assert 'Username already taken' in errors['username']
+    assert 'Email already taken' in errors['email']
