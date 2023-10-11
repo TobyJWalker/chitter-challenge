2ea7d72c5107fb79acd9cb91dea2556fc6189339
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-11 12:31:45 +0100

    Added home route

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ from lib.model_definition import User, Peep
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/', methods=['GET'])
+def index():
+    return redirect('/home')
 
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 
 # These lines start the server if you run this file directly
