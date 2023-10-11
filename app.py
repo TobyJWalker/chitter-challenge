ec8bbd25bfdf80aaa44854103a9f01e82d812cba
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-11 15:59:03 +0100

    added ability to search, implemented basic error page

--- app.py
+++ app.py
@@ -22,6 +22,32 @@ def home():
 
     return render_template('home.html', peeps=peeps, users=users)
 
+@app.route('/<user>', methods=['GET'])
+def get_user_peeps(user):
+    account = User.select().where(User.username == user).get()
+
+    peeps = sorted(Peep.select().where(Peep.user == account.id), key=lambda p: p.timestamp, reverse=True)
+
+    return render_template('user.html', peeps=peeps, user=account.username)
+
+@app.route('/search', methods=['POST'])
+def search_for_user():
+    username = request.form['username']
+
+    if username == '' or username.isspace():
+        return redirect('/home')
+    else:
+        try:
+            user = User.select().where(User.username == username).get()
+        except:
+            return render_template('error.html', error_title='User not found', error_msg=f'@{username} could not be found.')
+
+        return redirect(f'/{user.username}')
+
+@app.route('/signup', methods=['GET'])
+def get_signup_form():
+    return render_template('signup.html')
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
