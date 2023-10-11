3b51b5e60c5f27ced471a586c53f7fb9399ed8c3
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-11 11:25:19 +0100

    Updated app.py with imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os, peewee
 from flask import Flask, request, render_template, redirect
-from lib.database_connection import get_flask_database_connection
 from datetime import datetime
+from lib.model_definition import User, Peep
 
 # Create a new Flask app
 app = Flask(__name__)
